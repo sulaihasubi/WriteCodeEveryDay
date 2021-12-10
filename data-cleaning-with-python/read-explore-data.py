8bbfa74c6d666599df92ec201afad3b4bf483745
Sulaiha Subi <63158255+sulaihasubi@users.noreply.github.com>
2021-12-10 15:30:52 +0800

    Update read-explore-data.py

--- data-cleaning-with-python/read-explore-data.py
+++ data-cleaning-with-python/read-explore-data.py
@@ -2,4 +2,38 @@
 ### Goals: Read & Explore Data  ###
 ### Python Version: 3.9 ###
 
-# Import the library
+# import libraries
+import inline
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib
+plt.style.use('ggplot')
+from matplotlib.pyplot import figure
+
+%matplotlib inline
+matplotlib.rcParams['figure.figsize'] = (12,8)
+
+pd.options.mode.chained_assignment = None
+
+
+
+# read the data
+df = pd.read_csv('sberbank.csv')
+
+# shape and data types of the data
+print(df.shape)
+print(df.dtypes)
+
+# select numeric columns
+df_numeric = df.select_dtypes(include=[np.number])
+numeric_cols = df_numeric.columns.values
+print(numeric_cols)
+
+# select non numeric columns
+df_non_numeric = df.select_dtypes(exclude=[np.number])
+non_numeric_cols = df_non_numeric.columns.values
+print(non_numeric_cols)
